0bcfd3b6db691c00bfd74951bfd47ca4e6499f58
TenTen <compacten10@gmail.com>
2025-05-29 22:50:11 +0900

    ⚡️ バリデーション追加し、警告文表示機能の追加

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -11,17 +11,25 @@
         @csrf
         <div class="p-2 w-full">
           <div class="relative">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            <label for="name" class="leading-7 text-sm text-gray-600">カテゴリー名</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
+          <!-- カテゴリー名のバリデーションエラーメッセージの表示 -->
+          @error('name')
+          <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+          @enderror
         </div>
         <div class="p-2 w-full">
           <div class="relative">
-            <label for="description" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="description" name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            <label for="description" class="leading-7 text-sm text-gray-600">カテゴリー説明文</label>
+            <textarea id="description" name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            >{{ old('description') }}</textarea>
           </div>
+          <!-- カテゴリー説明文のバリデーションエラーメッセージの表示 -->
+          @error('description')
+          <p class="text-red-500 text-lg italic mt-2">{{ $message }}</p>
+          @enderror
         </div>
-        
         <div class="p-2 w-full">
           <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             登録
