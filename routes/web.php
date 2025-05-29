c5ca7a2d1b43ac30f1aadbcf9231265e077c27f4
TenTen <compacten10@gmail.com>
2025-05-29 22:21:36 +0900

    管理画面の編集と、登録画面の作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,9 +21,18 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::middleware('auth')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
   // 管理画面TOPページ
-    Route::get('/admin/top', function () {
+    Route::get('top', function () {
         return view('admin.top');
-    })->name('admin.top');
+    })->name('top');
+
+    // カテゴリ管理
+Route::prefix('categories')->name('categories.')->group(function () {
+  // カテゴリー新規登録画面
+  Route::get('/create', [CategoryController::class, 'create'])->name('create');
+// カテゴリー新規登録処理
+  Route::post('/store', [CategoryController::class, 'store'])->name('store');
+});
+
 });
