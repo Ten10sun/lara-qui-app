5e03b48f6d8f42296394707e58e066b854fbb189
TenTen <compacten10@gmail.com>
2025-06-07 14:53:17 +0900

    ⚡️ 解答画面の表示とその内容をDBから取得しチェック

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -4,55 +4,102 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Arr;
 
 class PlayController extends Controller
 {
-    /**
-     * プレイヤー画面のトップページを表示する
-     */
-    public function top(Request $request)
-    {
-        // カテゴリー一覧を取得
-        $categories = Category::all();
-        // ビューにカテゴリー一覧を渡す
-        return view('play.top', [
-            'categories' => $categories,
-        ]);
-    }
+  /**
+   * プレイヤー画面のトップページを表示する
+   */
+  public function top(Request $request)
+  {
+    // カテゴリー一覧を取得
+    $categories = Category::all();
+    // ビューにカテゴリー一覧を渡す
+    return view('play.top', [
+      'categories' => $categories,
+    ]);
+  }
 
-    // クイズスタート画面
-    public function categories(Request $request, int $categoryId)
-    {
+  // クイズスタート画面
+  public function categories(Request $request, int $categoryId)
+  {
 
-        // 指定されたカテゴリーIDのカテゴリーを取得
-        $category = Category::withCount('quizzes')->findOrFail($categoryId);
-        // dd($category->quizzes_count);
-        // ビューにカテゴリー情報を渡す
-        return view('play.start', [
-            'category' => $category,
-            'quizzesCount' => $category->quizzes_count,
-        ]);
-    }
+    // 指定されたカテゴリーIDのカテゴリーを取得
+    $category = Category::withCount('quizzes')->findOrFail($categoryId);
+    // dd($category->quizzes_count);
+    // ビューにカテゴリー情報を渡す
+    return view('play.start', [
+      'category' => $category,
+      'quizzesCount' => $category->quizzes_count,
+    ]);
+  }
 
-    // クイズ出題画面
-    public function quizzes(Request $request, int $categoryId)
-    {
-      // カテゴリーに紐づくクイズと選択肢を全て取得
-      $category = Category::with('quizzes.options')->findOrFail($categoryId);
-      // クイズをランダムに取得
-      $quizzes = $category->quizzes->toArray();
-      shuffle($quizzes); // 配列をシャッフル
-      $quiz = $quizzes[0];
-        // ビューにカテゴリー情報を渡す
-        // クイズ一覧を取得
-        // $quizzes = $category->quizzes()->inRandomOrder()->get();
-        // return view('play.quizzes', [
-        //     'category' => $category,
-        //     'quizzes' => $quizzes,
-        // ]);
-      return view('play.quizzes', [
-          'category' => $category,
-          'quiz' => $quiz,
-      ]);
+  // クイズ出題画面
+  public function quizzes(Request $request, int $categoryId)
+  {
+    // カテゴリーに紐づくクイズと選択肢を全て取得
+    $category = Category::with('quizzes.options')->findOrFail($categoryId);
+    // クイズをランダムに取得
+    $quizzes = $category->quizzes->toArray();
+    shuffle($quizzes); // 配列をシャッフル
+    $quiz = $quizzes[0];
+    // ビューにカテゴリー情報を渡す
+    // クイズ一覧を取得
+    // $quizzes = $category->quizzes()->inRandomOrder()->get();
+    // return view('play.quizzes', [
+    //     'category' => $category,
+    //     'quizzes' => $quizzes,
+    // ]);
+    return view('play.quizzes', [
+      'categoryId' => $categoryId,
+      'quiz' => $quiz,
+    ]);
+  }
+
+  // kクイズ解答画面
+  public function answer(Request $request, int $categoryId)
+  {
+    // dd($categoryId, $request);
+    $quizId = $request->quizId;
+    $selectedOptions = $request->optionId === null ? [] : (array)$request->optionId;
+
+    // カテゴリーに紐づくクイズと選択肢を全て取得
+    $category = Category::with('quizzes.options')->findOrFail($categoryId);
+    $quiz = $category->quizzes->firstWhere('id', $quizId);    
+    $quizOptions = $quiz->options->toArray();
+    $isCorrectAnswer = $this->isCorrectAnswer($selectedOptions, $quizOptions);
+    return view('play.answer'
+    , [
+      'categoryId' => $categoryId,
+      'quiz' => $quiz,
+      // 'quiz' => $quiz->toArray(),
+      'quizOptions' => $quizOptions,
+      'selectedOptions' => $selectedOptions,
+      'isCorrectAnswer' => $isCorrectAnswer,
+    ]
+  );
+  }
+
+  // プレイヤーの解答が正解か不正解かを判定する
+
+  private function isCorrectAnswer(array $selectedOptions, array $quizOptions)
+  {
+    // クイズの正解の選択肢を全て取得
+    $correctOptions = array_filter($quizOptions, function ($option) {
+      return $option['is_correct'] === 1;
+    });
+
+    // プレイヤーが選んだ正解の選択肢の個数と正解の選択肢の個数を比較
+    if (count($selectedOptions) !== count($correctOptions)) {
+      return false; // 不正解
     }
+    // 正解の選択肢IDとプレイヤーの選択肢IDが全て一致するかを比較
+    foreach ($correctOptions as $correctOption) {
+      if (!in_array($correctOption['id'], $selectedOptions)) {
+        return false; // 不正解
+      }
+    }
+    return true; // 正解
+  }
 }
--- resources/views/play/quizzes.blade.php
+++ resources/views/play/quizzes.blade.php
@@ -7,80 +7,45 @@
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">問題文</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ $quiz['question'] }}</p> 
     </div>
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-      <table class="table-auto w-full text-left whitespace-no-wrap">
-        <thead>
-          <tr>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">番号</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">選択肢</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
-          </tr>
-        </thead>
-        <tbody>
 
-          @for ($i = 0; $i < count($quiz['options']); $i++)
-            <tr>
-              <td class="px-4 py-3">{{ $i + 1 }}</td>
-              <td class="px-4 py-3">{{ $quiz['options'][$i]['content'] }}</td>
-              <td class="w-10 text-center">
-                <input name="plan" type="checkbox">
-              </td>
-            </tr>
-          @endfor
-          {{-- <tr>
-              <td class="px-4 py-3">1</td>
-              <td class="px-4 py-3">1,,,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque odit voluptatibus! 
-                Eos ullam   aliquid sequi maiores sit commodi, consectetur debitis tempora asperiores quo blanditiis facilis
-                voluptatibus doloribus possimus itaque.
-              </td>
-              <td class="w-10 text-center">
-                <input name="plan" type="checkbox">
-              </td>
-            </tr>
-          @endfor --}}
-          
-          {{-- <tr>
-            <td class="px-4 py-3">2</td>
-            <td class="px-4 py-3">2,,,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque odit voluptatibus! 
-              Eos ullam   aliquid sequi maiores sit commodi, consectetur debitis tempora asperiores quo blanditiis facilis
-              voluptatibus doloribus possimus itaque.
-            </td>
-            <td class="w-10 text-center">
-              <input name="plan" type="checkbox">
-            </td>
-          </tr>
-          <tr>
-            <td class="px-4 py-3">3</td>
-            <td class="px-4 py-3">3,,,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque odit voluptatibus! 
-              Eos ullam   aliquid sequi maiores sit commodi, consectetur debitis tempora asperiores quo blanditiis facilis
-              voluptatibus doloribus possimus itaque.
-            </td>
-            <td class="w-10 text-center">
-              <input name="plan" type="checkbox">
-            </td>
-          </tr>
-          <tr>
-            <td class="px-4 py-3">4
+    <form method="POST" action="{{ route('categories.quizzes.answer', ['categoryId' => $categoryId]) }}">
+        @csrf
+        {{-- クイズIDを送る --}}
+        <input type="hidden" name="quizId" value="{{ $quiz['id'] }}">
+        <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+          <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+              <tr>
+                <th class="whitespace-nowrap w-20 px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">番号</th>
+                <th class="whitespace-nowrap px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">選択肢</th>
+                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
+              </tr>
+            </thead>
+            <tbody>
 
-            </td>
-            <td class="px-4 py-3">4,,,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt atque odit voluptatibus! 
-              Eos ullam   aliquid sequi maiores sit commodi, consectetur debitis tempora asperiores quo blanditiis facilis
-              voluptatibus doloribus possimus itaque.
-            </td>
-            <td class="w-10 text-center">
-              <input name="plan" type="checkbox">
-            </td>
-          </tr> --}}
+              @for ($i = 0; $i < count($quiz['options']); $i++)
+                <tr>
+                  <td class="px-4 py-3 w-20">{{ $i + 1 }}</td>
+                  <td class="px-4 py-3">{{ $quiz['options'][$i]['content'] }}</td>
+                  <td class="w-10 text-center">
+                    <input name="optionId[]" type="checkbox" value="{{ $quiz['options'][$i]['id'] }}">
+                  </td>
+                </tr>
+              @endfor
 
-        </tbody>
-      </table>
-    </div>
-    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-      <button 
-        class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-        解答
-      </button>
-    </div>
+
+            </tbody>
+          </table>
+        </div>
+        <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+          <button 
+            type="submit"
+            class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+            解答
+          </button>
+        </div>
+    </form>
+
   </div>
 </section>
 </x-play-layout>
